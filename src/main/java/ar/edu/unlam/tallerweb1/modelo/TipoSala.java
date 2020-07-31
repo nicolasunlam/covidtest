c5b2de9f7b4dc7dd00e74ef870f79d2d9c380b3e
Lucho F <lucianofiamingo@gmail.com>
2020-07-31 02:46:43 -0300

    getters en enums para traer descripcion bien escrita

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/TipoSala.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/TipoSala.java
@@ -3,6 +3,20 @@ package ar.edu.unlam.tallerweb1.modelo;
 
 public enum TipoSala {
 	
-	TERAPIA_INTENSIVA, TERAPIA_INTERMEDIA, SALA_AISLAMIENTO,
-	HABITACION_HOTEL, AULA_UNIVERSITARIA, CLUB;
+	TERAPIA_INTENSIVA("Terapia Intensiva"), 
+	TERAPIA_INTERMEDIA("Terapia Intermedia"), 
+	SALA_AISLAMIENTO("Sala de Aislamiento"),
+	HABITACION_HOTEL("Habitaciión de Hotel"), 
+	AULA_UNIVERSITARIA("Aula Universitaria"), 
+	CLUB("Club");
+	
+    private final String descripcion;
+
+    private TipoSala (String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
