6a2fb0a5509e71f0c8a567523319c4f560e21f58
JuanPabloSaladino <juanpablosaladino@gmail.com>
2020-08-22 22:59:54 -0300

    Se modifico la vista admin y ControladorAdmin

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
@@ -11,11 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Institucion;
 import ar.edu.unlam.tallerweb1.modelo.Paciente;
+import ar.edu.unlam.tallerweb1.modelo.Piso;
 import ar.edu.unlam.tallerweb1.modelo.Rol;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioAtajo;
+import ar.edu.unlam.tallerweb1.servicios.ServicioCama;
+import ar.edu.unlam.tallerweb1.servicios.ServicioInstitucion;
 import ar.edu.unlam.tallerweb1.servicios.ServicioPaciente;
+import ar.edu.unlam.tallerweb1.servicios.ServicioPiso;
 import ar.edu.unlam.tallerweb1.servicios.ServicioUsuario;
 
 @Controller
@@ -26,54 +31,98 @@ public class ControladorAdmin {
 	@Autowired
 	private ServicioPaciente servicioPaciente;
 	@Autowired
-	private ServicioAtajo servicioAtajo;	
-	
+	private ServicioAtajo servicioAtajo;
+	@Autowired
+	private ServicioCama servicioCama;
+	@Autowired
+	private ServicioPiso servicioPiso;
+	@Autowired
+	private ServicioInstitucion servicioInstitucion;
+
 	@RequestMapping("admin")
-	public ModelAndView irAadmin (HttpServletRequest request) {
+	public ModelAndView irAadmin(HttpServletRequest request) {
 
 		ModelMap model = new ModelMap();
 
-		if(servicioAtajo.validarInicioDeSesion(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
-    	}
-    	if(servicioAtajo.validarPermisoAPagina(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
-    	}
-    	Rol rol = (Rol) request.getSession().getAttribute("ROL");
-		if(rol != null) {
-			model.put("rol", rol.name());	
+		if (servicioAtajo.validarInicioDeSesion(request) != null) {
+			return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
 		}
-    	model.put("armarHeader", servicioAtajo.armarHeader(request));
+		if (servicioAtajo.validarPermisoAPagina(request) != null) {
+			return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
+		}
+		Rol rol = (Rol) request.getSession().getAttribute("ROL");
+		if (rol != null) {
+			model.put("rol", rol.name());
+		}
+		model.put("armarHeader", servicioAtajo.armarHeader(request));
 
-    	Long id = (long) request.getSession().getAttribute("ID"); 
+		Long id = (long) request.getSession().getAttribute("ID");
 		Usuario admin = servicioUsuario.consultarUsuarioPorId(id);
-		
+
 		String nombre = admin.getNombre();
-		
-		model.put("nombre", nombre);
+
+		model.put("nomb-re", nombre);
+
+		/**/
+
+		Integer cantidadTotalCamas = servicioCama.obtenerCamas().size();
+		Integer cantidadCamasOcupadas = servicioCama.obtenerTotalDeCamasOcupadas().size();
+		Integer cantidadCamasDisponibles = servicioCama.obtenerTotalDeCamasDisponibles().size();
+		Integer cantidadCamasReservadas = cantidadTotalCamas - (cantidadCamasDisponibles + cantidadCamasOcupadas);
+
+		model.put("cantidadTotalCamas", cantidadTotalCamas);
+		model.put("cantidadCamasOcupadas", cantidadCamasOcupadas);
+		model.put("cantidadCamasDisponibles", cantidadCamasDisponibles);
+		model.put("cantidadCamasReservadas", cantidadCamasReservadas);
+		/**/
+
+		/*
+		 * List<Institucion> listaInstituciones =
+		 * servicioInstitucion.obtenerListaInstituciones();
+		 * 
+		 * Integer cantidadCamasReservadas = 0; Integer cantidadCamasOcupadas = 0;
+		 * Integer cantidadCamasDisponibles = 0;
+		 * 
+		 * for (Institucion institucion : listaInstituciones) {
+		 * 
+		 * List<Piso> pisosInstitucion =
+		 * servicioPiso.listarPisosPorInstitucion(institucion);
+		 * 
+		 * for (Piso piso : pisosInstitucion) { cantidadCamasReservadas +=
+		 * servicioCama.listarCamasReservadasPorPiso(piso).size();
+		 * cantidadCamasDisponibles +=
+		 * servicioCama.listarCamasDisponiblesPorPiso(piso).size();
+		 * cantidadCamasOcupadas +=
+		 * servicioCama.listarCamasOcupadasPorPiso(piso).size(); } }
+		 * 
+		 * model.put("cantidadCamasOcupadas", cantidadCamasOcupadas);
+		 * model.put("cantidadCamasDisponibles", cantidadCamasDisponibles);
+		 * model.put("cantidadCamasReservadas", cantidadCamasReservadas);
+		 */
+		/**/
 
 		return new ModelAndView("admin", model);
 	}
-	
+
 	@RequestMapping("homeAdmin")
 	public ModelAndView irAhomeAdmin(HttpServletRequest request) {
-		
+
 		ModelMap model = new ModelMap();
 
-		if(servicioAtajo.validarInicioDeSesion(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
-    	}
-    	if(servicioAtajo.validarPermisoAPagina(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
-    	}
-    	Rol rol = (Rol) request.getSession().getAttribute("ROL");
-		if(rol != null) {
-			model.put("rol", rol.name());	
+		if (servicioAtajo.validarInicioDeSesion(request) != null) {
+			return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
 		}
-    	model.put("armarHeader", servicioAtajo.armarHeader(request));
-			
+		if (servicioAtajo.validarPermisoAPagina(request) != null) {
+			return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
+		}
+		Rol rol = (Rol) request.getSession().getAttribute("ROL");
+		if (rol != null) {
+			model.put("rol", rol.name());
+		}
+		model.put("armarHeader", servicioAtajo.armarHeader(request));
+
 		return new ModelAndView("homeAdmin", model);
-		
+
 	}
 
 	@RequestMapping("/panel")
@@ -81,17 +130,17 @@ public class ControladorAdmin {
 
 		ModelMap model = new ModelMap();
 
-		if(servicioAtajo.validarInicioDeSesion(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
-    	}
-    	if(servicioAtajo.validarPermisoAPagina(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
-    	}
-    	Rol rol = (Rol) request.getSession().getAttribute("ROL");
-		if(rol != null) {
-			model.put("rol", rol.name());	
+		if (servicioAtajo.validarInicioDeSesion(request) != null) {
+			return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
 		}
-    	model.put("armarHeader", servicioAtajo.armarHeader(request));
+		if (servicioAtajo.validarPermisoAPagina(request) != null) {
+			return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
+		}
+		Rol rol = (Rol) request.getSession().getAttribute("ROL");
+		if (rol != null) {
+			model.put("rol", rol.name());
+		}
+		model.put("armarHeader", servicioAtajo.armarHeader(request));
 
 		List<Paciente> poInfectados = servicioPaciente.posiblesInfectados();
 
@@ -101,42 +150,42 @@ public class ControladorAdmin {
 
 	}
 
-	//PAGINA DE ACCESO DENEGADO POR DEFAULT
+	// PAGINA DE ACCESO DENEGADO POR DEFAULT
 	@RequestMapping(path = "/denied", method = RequestMethod.GET)
 	public ModelAndView denied(
-			
+
 			HttpServletRequest request) {
-		
+
 		ModelMap model = new ModelMap();
 
-		if(servicioAtajo.validarInicioDeSesion(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
-    	}
-    	if(servicioAtajo.validarPermisoAPagina(request) != null) {
-    		return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
-    	}
-    	Rol rol = (Rol) request.getSession().getAttribute("ROL");
-		if(rol != null) {
-			model.put("rol", rol.name());	
+		if (servicioAtajo.validarInicioDeSesion(request) != null) {
+			return new ModelAndView(servicioAtajo.validarInicioDeSesion(request));
 		}
-    	model.put("armarHeader", servicioAtajo.armarHeader(request));
+		if (servicioAtajo.validarPermisoAPagina(request) != null) {
+			return new ModelAndView(servicioAtajo.validarPermisoAPagina(request));
+		}
+		Rol rol = (Rol) request.getSession().getAttribute("ROL");
+		if (rol != null) {
+			model.put("rol", rol.name());
+		}
+		model.put("armarHeader", servicioAtajo.armarHeader(request));
 
 		return new ModelAndView("denied", model);
 	}
-	
-	//PAGINA DE ACCESO DENEGADO POR DEFAULT
+
+	// PAGINA DE ACCESO DENEGADO POR DEFAULT
 	@RequestMapping("/acordeon")
 	public ModelAndView acordeon(
-			
+
 			HttpServletRequest request) {
-		
+
 		ModelMap model = new ModelMap();
 
-    	Rol rol = (Rol) request.getSession().getAttribute("ROL");
-		if(rol != null) {
-			model.put("rol", rol.name());	
+		Rol rol = (Rol) request.getSession().getAttribute("ROL");
+		if (rol != null) {
+			model.put("rol", rol.name());
 		}
-    	model.put("armarHeader", servicioAtajo.armarHeader(request));
+		model.put("armarHeader", servicioAtajo.armarHeader(request));
 
 		return new ModelAndView("acordeon", model);
 	}
--- src/main/webapp/WEB-INF/vistas/admin.jsp
+++ src/main/webapp/WEB-INF/vistas/admin.jsp
@@ -5,14 +5,51 @@
 
 <jsp:include page="../../partial/${armarHeader}1.jsp" />
 
-<title>Inserte titulo aquí</title>
+<title>Home Admin</title>
 
 <jsp:include page="../../partial/${armarHeader}2.jsp" />
 
+<script>
+	window.onload = function() {
+		alert(${cantidadTotalCamas});		
+		alert(${cantidadCamasDisponibles});
+		alert(${cantidadCamasOcupadas});
+		alert(${cantidadCamasReservadas});
+		
+
+		//Grafico de barras - Cantidad de camas ocupadas
+ 		var chart = new CanvasJS.Chart(
+				"chartContainerBarras",
+				{
+					animationEnabled : true,
+					theme : "dark2", // "light1", "light2", "dark1", "dark2",
+					title : {
+						text : "Cantidad total de camas ocupadas, disponibles y reservadas",
+						fontFamily : "calibri light",
+					},
+					axisY : {
+						title : "Cantidad total de camas"
+					},
+					data : [ {
+						type : "column",
+						dataPoints : [ 
+						{y: ${cantidadCamasOcupadas}, label: "Ocupadas", color:"#FE2E2E"},
+						{y: ${cantidadCamasDisponibles}, label: "Disponibles", color: "#2E9AFE"},
+						{y: ${cantidadCamasReservadas}, label: "Reservadas", color: "#F4FA58"}
+						]
+					} ]
+				});
+
+		chart.render();	  
+		 
+
+	}
+</script>
+
 <div class="jumbotron">
 	<h1 class="display-4">Bienvenido/a</h1>
-	<p class="my-4 lead">Desde aquí acceda a las opciones principales de:
-		ADMINISTRADOR</p>
+	<p class="my-4 lead">Desde aquí acceda a las opciones principales
+		de: ADMINISTRADOR</p>
 	<a class="btn btn-outline-primary" href="verMensajes" role="button">Ver
 		Mensajes Recibidos</a> <a class="btn btn-outline-primary"
 		href="verMensajesEnviados" role="button">Ver Mensajes Enviados</a>
@@ -21,10 +58,26 @@
 		<a class="btn btn-primary btn-lg" href="listaPacientesInternados"
 			role="submit"> Panel de Control</a>
 	</p>
+
+	<div
+		class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 pb-2 mb-2 border-bottom">
+		<h1 class="display-4">Reportes de camas</h1>
+
+	</div>
+	<div class="mt-4">
+		<div class="row container-chart">
+			<div class="col-2"></div>
+			<div class="col-8">
+				<div id="chartContainerBarras" style="height: 30em; width: 100%;"></div>
+			</div>
+			<div class="col-2"></div>
+		</div>
+	</div>
+
 </div>
 
-<%@ include file="../../partial/footer.jsp" %>
-
+<%@ include file="../../partial/footer.jsp"%>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 
 </html>
